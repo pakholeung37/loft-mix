ffc09de9fd4beaeaa764405f91cc2918cde07168
Pakho Leung <pakholeung37@gmail.com>
2022-05-22 19:58:50 +0800

    chore: add tailwind

--- packages/main/src/App.tsx
+++ packages/main/src/App.tsx
@@ -1,18 +1,13 @@
 import type { Component } from 'solid-js'
 
-import logo from './logo.svg'
-import styles from './App.module.css'
-
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
+    <div class="m-0">
+      <header>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          class={styles.link}
           href="https://github.com/solidjs/solid"
           target="_blank"
           rel="noopener noreferrer"
---
+++ packages/main/tailwind.config.js
@@ -0,0 +1,5 @@
+const config = require('../../tailwind.config')
+
+module.exports = {
+  ...config,
+}
