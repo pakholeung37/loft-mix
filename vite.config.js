452bb931f984ecee9274dbeafd51783df1a71e1f
Pakho Leung <pakholeung37@gmail.com>
2022-06-23 19:27:16 +0800

    feat: expose bundle stat via /stat

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 const { defineConfig } = require('vite')
 const solidPlugin = require('vite-plugin-solid')
 const { vanillaExtractPlugin } = require('@vanilla-extract/vite-plugin')
-const { visualizer } = require("rollup-plugin-visualizer");
+const { visualizer } = require('rollup-plugin-visualizer')
 
 module.exports = defineConfig({
-  plugins: [solidPlugin(), vanillaExtractPlugin(), visualizer()],
+  plugins: [
+    solidPlugin(),
+    vanillaExtractPlugin(),
+    visualizer({ filename: './dist/stat.html' }),
+  ],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
